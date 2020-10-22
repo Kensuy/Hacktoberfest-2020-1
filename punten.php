620091580308c75824c068a5d5ad4ae99fe6b0c1
zxha <50351446+zxha@users.noreply.github.com>
2020-10-22 12:55:30 +0700

    Update punten.php

--- punten.php
+++ punten.php
@@ -8,8 +8,7 @@
     <title>P</title>
 </head>
 <body>
-<div class="alert alert-primary" role="alert">
-  A simple primary alert—check it out!
+
 </div>
 <div class="alert alert-secondary" role="alert">
   A simple secondary alert—check it out!
